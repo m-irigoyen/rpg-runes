d01ea6e9a2c9ac0539bd6d48acc5ecc4458d1123
Milan "Edrevan" Irigoyen <edrevan@gmail.com>
2016-02-26 00:59:29 +0100

    Working version of qt build script (woohoo!)

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,7 @@ using namespace Runes;
 
 int main()
 {
-	xml_document doc;
-	xml_node& node = doc.append_child("runes").append_child("rune");
-	node.append_attribute("index").set_value(0);
-	node.append_attribute("name").set_value("Speed");
-
-	Serializable::writeFile(&doc, "test", Paths::SPELLS);
+	/*QApplication app;*/
 	
 	return 0;
 }
